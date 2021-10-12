07d15c0dc88ec22e0dd0dd990b485f86c8b1de5a
LamNguyen2608 <phuclam.work@gmail.com>
2021-10-12 23:42:12 +0700

    hello

---
+++ new.js
@@ -0,0 +1,61 @@
+const http = require("http");
+var fs = require('fs');
+const hostname = "localhost";
+const port = process.env.PORT || 3000;
+var solan_home = 0;
+var solan_about = 0;
+const server = http.createServer((req, res) => {
+res.setHeader("Content-Type","text/html");
+res.statusCode = 200;
+switch (req.url){
+    case "/home":
+        res.writeHead(200);
+        var data =fs.readFileSync('./home.html');
+        res.end(data.toString());
+        console.log("--- request home page !!!", solan_home++);
+        break;
+
+    case "/product":
+        res.writeHead(200);
+        var product =fs.readFileSync('./product.html');
+        res.end(product.toString());
+        break;
+
+    case "/car":
+        res.writeHead(200);
+        var Car =fs.readFileSync('./car.html');
+   
+        res.end(Car.toString());
+        break;
+     case "/airplane":
+            res.writeHead(200);
+            var Airplane =fs.readFileSync('./airplane.html');
+       
+        res.end(Airplane.toString());
+            break;
+
+    case "/about":
+        res.writeHead(200);
+        var about =fs.readFileSync('./about.html');          
+        res.end(about.toString());
+        break;
+    case "/login":
+        res.writeHead(200);
+        var about =fs.readFileSync('./User_login.html');          
+        res.end(about.toString());
+        break;
+    case "/signup":
+        res.writeHead(200);
+        var about =fs.readFileSync('./Signup.html');          
+        res.end(about.toString());
+        break;
+    default:
+            res.writeHead(200);
+         res.end("ERROR");
+            console.log("--- ERROR !!!");
+        break;
+}
+});
+server.listen( port,() => {
+console.log("Listen !!! " ,port);
+});
